b21aef2e6b54f828f02221dc1c71af7aa8f4474b
Juliano Teles Tolomini Biasotto <julianobiasotto@gmail.com>
2026-04-10 15:53:35 -0300

    Alterando a porta para a porta padrão

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
-    port: 3307,
+    port: 3306,
     user: user,
     password: password,
     database: "videogamesdb"
